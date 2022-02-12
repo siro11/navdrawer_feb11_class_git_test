23c03902e9ba807b63c9005b0085a4900d2a0aba
Siro Rajnath <sirorajnath1999@gmail.com>
2022-02-12 16:01:49 -0600

    spaces in main

--- app/src/main/java/com/example/navdrawer_feb11_class_git_test/MainActivity.kt
+++ app/src/main/java/com/example/navdrawer_feb11_class_git_test/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
